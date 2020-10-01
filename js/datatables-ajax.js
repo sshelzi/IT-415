749f49bd8fc366a4e31bd733154ea49f180b37bd
sshelzi <sabrinashelzi@aol.com>
2020-10-01 13:46:08 -0400

    Update datatables-ajax.js

--- js/datatables-ajax.js
+++ js/datatables-ajax.js
@@ -3,7 +3,49 @@ $(document).ready(function() {
         ajax: 'ajax/gas-stations.txt',
         columns: [
             {
+                className: 'logo'
                 data: 'name'
+                render: function(data, type) {
+                    if (type === 'display') {
+                        var logo = '';
+ 
+                        switch (data) {
+                            case 'CITGO':
+                                logo = 'CITGO';
+                                break;
+                            case 'Irving':
+                                logo = 'Irving';
+                                break;
+                            case 'Mobil':
+                                logo = 'Mobil';
+                                break;
+                            case 'Cumberland Farms':
+                                logo = 'Cumberland Farms';
+                                break;
+                            case 'Sunoco':
+                                logo = 'Sunoco';
+                                break;
+							case 'Sams Club':
+                                logo = 'Sams Club';
+                                break;
+							case 'BJs':
+                                logo = 'BJs';
+                                break;
+							case 'Shell':
+                                logo = 'Shell';
+                                break;
+							case 'Gulf':
+                                logo = 'Gulf';
+                                break;
+                        }
+ 
+                        return '<span class="logo ' + logo + '"></span> ' + data;
+						//return '<img src="" />' + data;
+			
+                    }
+ 
+                    return data;
+                }
             },
             {
                 data: 'position'
@@ -61,3 +103,20 @@ $(document).ready(function() {
         ]
     });
 });
+
+
+
+
+
+//show elips  for long strings
+//$('#example').dataTable( {
+//  "columnDefs": [ {
+//    "targets": 4,
+//    "data": "description",
+//    "render": function ( data, type, row, meta ) {
+//      return type === 'display' && data.length > 40 ?
+//        '<span title="'+data+'">'+data.substr( 0, 38 )+'...</span>' :
+//        data;
+//    }
+//  } ]
+//} );
